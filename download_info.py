79a3bd1857aff80c4ca8fedee4e74b4378a2ec24
Luis <lhlopezm2@gmail.com>
2023-07-29 18:15:06 -0500

    Apply flake8 to download_info.py

--- download_info.py
+++ download_info.py
@@ -5,105 +5,145 @@ import numpy as np
 import requests
 from auxiliary_functions.Web_genome import download
 
-# Returns the path of the gff file for a given a species name and the path where the gffs are located
+
 def find(name, path):
-  for root, dirs, files in os.walk(path, topdown=True):
-    if name in files:
-      return os.path.join(root, name)
-  return None
+    """Returns the path of the gff file for a given a species
+    name and the path where the gffs are located """
+    for root, _, files in os.walk(path, topdown=True):
+        if name in files:
+            return os.path.join(root, name)
+    return None
 
-# Returns the size in MB of the genomes to be downloaded
+
 def real_size(row):
-  try:
-    number = requests.get(row['Filtered data'], stream=True, timeout = 5).headers.get("Content-length")
-    if number == None:
-      number = 0
-  except:
-    number = 0
-  return float(number)/(1024**2)
+    """Returns the size in MB of the genomes to be downloaded"""
+    try:
+        headers = requests.get(row['Filtered data'], stream=True, timeout=5).headers
+        number = headers.get("Content-length")
+        if number is None:
+            number = 0
+    except Exception:  # pylint: disable=broad-except
+        number = 0
+    return float(number)/(1024**2)
 
-# Returns a dictionary using the id as the key and the sequence as the value
+
 def genoma_dicc(file):
-  parser = list(SeqIO.parse(file, 'fasta'))
-  if ' ' in parser[0].id:
-    genoma = [(x.id.split(' ')[0], x.seq) for x in parser]
-  else:
-    genoma = [(x.id, x.seq) for x in parser]
-  genoma = {x:y for (x,y) in genoma}
-  return genoma
+    """Returns a dictionary using the id as the key and the sequence as the value"""
+    parser = list(SeqIO.parse(file, 'fasta'))
+    if ' ' in parser[0].id:
+        genoma = [(x.id.split(' ')[0], x.seq) for x in parser]
+    else:
+        genoma = [(x.id, x.seq) for x in parser]
+    genoma = {x: y for (x, y) in genoma}
+    return genoma
 
-# Writes a fasta file with only the TE annotated in the gffs.
-def TE_extraction(fasta, gff, TE_file):
-  genome = genoma_dicc(fasta)
-  df = pd.read_csv(gff,sep='\t', dtype={'Lineages': 'str', 'Divergence': 'str'})
-  df_grouped = df.groupby('LTR_ID')
-  with open (TE_file, 'a') as myfile:
-    for id, data in df_grouped:
-      data.sort_values(['Start'], inplace=True)
-      start = np.array(data["Start"])
-      end = np.array(data["End"])
-      sf = np.array(data["Superfamily"])
-      dom = np.array(data["Domain"])
-      species = np.array(data["Species"])
-      chrom = np.array(data["Chromosome"])
-      try:
-        a = genome[chrom[0]]
-      except:
-        print(f"Genoma {gff} with Chromosome column {chrom[0]} is not matching chromosome id in fasta")
-        continue
-      domains={}
-      TE_dicc = {'TE': [start[0],end[-1]]}
-      dicc = {'intact_5ltr':'LTR','RH':'RH','RT':'RT','INT':'INT','PROT':'PROT','GAG':'GAG','intact_3ltr':'LTR'}
-      for i in range(len(start)):
-        try:
-          if dicc[dom[i]] in domains:
-            domains[dicc[dom[i]]]+=[start[i],end[i]]
-          else:
-            domains[dicc[dom[i]]]=[start[i],end[i]]
-        except:
-          pass
-      myfile.write(f">{species[0]}#{chrom[0]}#{sf[0]}#{str(TE_dicc)}#{str(domains)}\n")
-      myfile.write(str(genome[chrom[0]][start[0]:end[-1]])+"\n")
-  return None
 
-# Writes a csv file with the real size of the genomes to be downloaded and returns it as a pandas obejct
+def te_extraction(fasta, gff, te_file):
+    """Writes a fasta file with only the TE annotated in the gffs"""
+    genome = genoma_dicc(fasta)
+    df_gff = pd.read_csv(gff, sep='\t', dtype={'Lineages': 'str', 'Divergence': 'str'})
+    df_grouped = df_gff.groupby('LTR_ID')
+    with open(te_file, 'a', encoding="utf-8") as myfile:
+        for _, data in df_grouped:
+            data_dicc = extract_data(data)
+            try:
+                _ = genome[data_dicc["chrom"][0]]
+            except Exception:  # pylint: disable=broad-except
+                print(f"Genoma {gff} with Chromosome column {data_dicc['chrom'][0]} is not \
+                      matching chromosome id in fasta")
+                continue
+            domains = {}
+            te_dicc = {'TE': [data_dicc["start"][0], data_dicc["end"][-1]]}
+            dicc = {'intact_5ltr': 'LTR', 'RH': 'RH', 'RT': 'RT', 'INT': 'INT', 'PROT': 'PROT',
+                    'GAG': 'GAG', 'intact_3ltr': 'LTR'}
+            for i, start_i in enumerate(data_dicc["start"]):
+                try:
+                    if dicc[data_dicc["dom"][i]] in domains:
+                        domains[dicc[data_dicc["dom"][i]]] += [start_i, data_dicc["end"][i]]
+                    else:
+                        domains[dicc[data_dicc["dom"][i]]] = [start_i, data_dicc["end"][i]]
+                except Exception:  # pylint: disable=broad-except
+                    pass
+            myfile.write(f">{data_dicc['species'][0]}#{data_dicc['chrom'][0]}#\
+                         {data_dicc['s_f'][0]}#{str(te_dicc)}#{str(domains)}\n")
+            interval = [data_dicc['start'][0], data_dicc['end'][-1]]
+            myfile.write(str(genome[data_dicc['chrom'][0]][interval[0]:interval[1]])+"\n")
+
+
+def extract_data(data):
+    """Extracts start, end, sf, dom, species and chrom from data"""
+    data_dicc = {}
+    data.sort_values(["Start"], inplace=True)
+    data_dicc["start"] = np.array(data["Start"])
+    data_dicc["end"] = np.array(data["End"])
+    data_dicc["s_f"] = np.array(data["Superfamily"])
+    data_dicc["dom"] = np.array(data["Domain"])
+    data_dicc["species"] = np.array(data["Species"])
+    data_dicc["chrom"] = np.array(data["Chromosome"])
+    return data_dicc
+
+
 def obtain_genome_size():
-  if os.path.exists('./data/dataset.csv'):
-    df = pd.read_csv('./data/dataset.csv')
-    print('dataset.csv ya existe')
-  else:
-    df = pd.read_csv('data/genomes_links.csv', sep = ";")
-    df = df[['Order','Family','Species','Filtered data','genome_size']]
-    df.sort_values(['genome_size'] , inplace = True)
-    df['real_size']= df.apply(lambda row: real_size(row), axis=1)
-    print(f"El numero de links es de {df['real_size'].count()}")
-    df = df[df['real_size'] > 0]
-    print(f"El numero de links accesibles es de {df['real_size'].count()}")
-    df.sort_values(by=['real_size'], ascending=True, inplace = True)
-    df.reset_index(inplace = True, drop = True)
-    df.reset_index(inplace=True)
-    df['fasta_name'] = df.apply(lambda row: 'Fasta'+str(row['index'])+'.fasta', axis=1)
-    df['path_annot'] = df.apply(lambda row: find(row['Species'].replace(' ','_')+'.txt', 'data/dataset_intact_LTR-RT'), axis=1)
-    df.to_csv('./data/dataset.csv', index = False)
-  print(df[['Species','path_annot']].head(5))
-  return df
+    """Writes a csv file with the real size of the genomes to be downloaded
+    and returns it as a pandas obejct"""
+    if os.path.exists('./data/dataset.csv'):
+        df_dataset = pd.read_csv('./data/dataset.csv')
+        print('dataset.csv ya existe')
+    else:
+        df_dataset = pd.read_csv('data/genomes_links.csv', sep=";")
+        df_dataset = df_dataset[['Order', 'Family', 'Species', 'Filtered data', 'genome_size']]
+        df_dataset.sort_values(['genome_size'], inplace=True)
+        df_dataset['real_size'] = df_dataset.apply(real_size, axis=1)
+        print(f"El numero de links es de {df_dataset['real_size'].count()}")
+        df_dataset = df_dataset[df_dataset['real_size'] > 0]
+        print(f"El numero de links accesibles es de {df_dataset['real_size'].count()}")
+        df_dataset.sort_values(by=['real_size'], ascending=True, inplace=True)
+        df_dataset.reset_index(inplace=True, drop=True)
+        df_dataset.reset_index(inplace=True)
+        df_dataset['fasta_name'] = df_dataset.apply(
+            lambda row: 'Fasta'+str(row['index'])+'.fasta',
+            axis=1
+        )
+        df_dataset['path_annot'] = df_dataset.apply(
+            lambda row: find(row['Species'].replace(' ', '_')+'.txt', 'data/dataset_intact_LTR-RT'),
+            axis=1
+        )
+        df_dataset.to_csv('./data/dataset.csv', index=False)
+    print(df_dataset[['Species', 'path_annot']].head(5))
+    return df_dataset
 
-# Builds the fasta file iterating over all the genomes
-def build_te_fasta(df):
-  current_index = -1
-  if os.path.exists(f"./data/TEDB.fasta"):
-    lista = list(SeqIO.parse(f"./data/TEDB.fasta", 'fasta'))
-    name = str(lista[-1].id).split('#')[0].replace('_',' ')
-    current_index = df[df['Species']==name]['index'].iloc[0]
-  for index, row in df.iterrows():
-    if index > current_index:
-      if not os.path.exists(f"{row['fasta_name']}"):
-        download(path_save = '.', link = row['Filtered data'],name = row['fasta_name'],timeout = 100)
-      TE_extraction(fasta = row['fasta_name'],gff = row['path_annot'], TE_file = 'data/TEDB.fasta')
-      os.remove(row['fasta_name'])
-    #if index == 1:
-    #  break
-  return None
 
-df = obtain_genome_size()
-build_te_fasta(df)
+def build_te_fasta(df_with_size):
+    """Builds the fasta file iterating over all the genomes"""
+    current_index = -1
+    if os.path.exists("./data/TEDB.fasta"):
+        lista = list(SeqIO.parse("./data/TEDB.fasta", 'fasta'))
+        name = str(lista[-1].id).split('#', maxsplit=1)[0].replace('_', ' ')
+        current_index = df_with_size[df_with_size['Species'] == name]['index'].iloc[0]
+    for index, row in df_with_size.iterrows():
+        if index > current_index:
+            if not os.path.exists(f"{row['fasta_name']}"):
+                download(
+                    path_save='.',
+                    link=row['Filtered data'],
+                    name=row['fasta_name'],
+                    timeout=100
+                )
+            te_extraction(
+                fasta=row['fasta_name'],
+                gff=row['path_annot'],
+                te_file='data/TEDB.fasta'
+            )
+            os.remove(row['fasta_name'])
+        # if index == 1:
+        #  break
+
+
+def main():
+    """Creates the dataset.csv and genomes_link.csv"""
+    df_with_size = obtain_genome_size()
+    build_te_fasta(df_with_size)
+
+
+if __name__ == "__main__":
+    main()
