11427b28444b8bbf8e82b47f3135dcb4fde4555e
Luis <lhlopezm2@gmail.com>
2023-07-29 23:53:12 -0500

    Organize code into functions

--- npy_generation.py
+++ npy_generation.py
@@ -2,74 +2,73 @@
 import numpy as np
 import random
 import json
+import gc
 from sklearn.model_selection import train_test_split
 from Bio import SeqIO
 random.seed(10)
 
-# PRELIMINAR ANALYSIS OF DOMAINS
-
-rec_TE = list(SeqIO.parse("TEDB.fasta", "fasta"))
-rec_non = list(SeqIO.parse("negative.fasta", "fasta"))
-
-dicc_analysis = {'TE': [], 'TE2': [], 'PROT': [], 'RT': [], 'INT': [], 'RH': [],
-                 'GAG': [], 'LTR': [], 'internal': []}
-for i in rec_TE:
-    dicc_tmp_dom = json.loads(i.description.split("#")[-1].replace('\'', '\"'))
-    dicc_tmp_te = json.loads(i.description.split("#")[-2].replace('\'', '\"'))
-    dicc_analysis['TE'].append(dicc_tmp_te['TE'][1]-dicc_tmp_te['TE'][0]+1)
-    dicc_analysis['TE2'].append(len(i.seq))
-    internal_list = []
-    for key, value in dicc_tmp_dom.items():
-        if key != 'LTR':
-            internal_list += value
-        if len(value) == 2:
-            dicc_analysis[key].append(value[-1]-value[0]+1)
-        else:
-            dicc_analysis[key].append(value[1]-value[0]+1)
-            dicc_analysis[key].append(value[3]-value[2]+1)
-    try:
-        dicc_tmp_dom['LTR']
-    except Exception:  # pylint: disable=broad-except
-        print(f'{i.description}')
-    start_internal = min(internal_list)
-    end_internal = max(internal_list)
-    dicc_analysis['internal'].append(end_internal-start_internal+1)
-
-print(f"La cantidad total de RT es de {len(dicc_analysis['RT'])}")
-print(f"La cantidad total de GAG es de {len(dicc_analysis['GAG'])}")
-print(f"La cantidad total de INT es de {len(dicc_analysis['INT'])}")
-print(f"La cantidad total de PROT es de {len(dicc_analysis['PROT'])}")
-print(f"La cantidad total de RH es de {len(dicc_analysis['RH'])}")
-print(f"La cantidad total de LTR es de {len(dicc_analysis['LTR'])}")
-
-print(f"El tamaño máximo del RT es {max(dicc_analysis['RT'])}")
-print(f"El tamaño máximo del GAG es {max(dicc_analysis['GAG'])}")
-print(f"El tamaño máximo del INT es {max(dicc_analysis['INT'])}")
-print(f"El tamaño máximo del PROT es {max(dicc_analysis['PROT'])}")
-print(f"El tamaño máximo del RH es {max(dicc_analysis['RH'])}")
-print(f"El tamaño máximo del LTR es {max(dicc_analysis['LTR'])}")
-
-print(f"El tamaño mínimo del RT es {min(dicc_analysis['RT'])}")
-print(f"El tamaño mínimo del GAG es {min(dicc_analysis['GAG'])}")
-print(f"El tamaño mínimo del INT es {min(dicc_analysis['INT'])}")
-print(f"El tamaño mínimo del PROT es {min(dicc_analysis['PROT'])}")
-print(f"El tamaño mínimo del RH es {min(dicc_analysis['RH'])}")
-print(f"El tamaño mínimo del LTR es {min(dicc_analysis['LTR'])}")
-
-print(f"El tamaño máximo del internal es {max(dicc_analysis['internal'])}")
-print(f"El tamaño mínimo del internal es {min(dicc_analysis['internal'])}")
-
-print(f"El tamaño máximo del TE es {max(dicc_analysis['TE'])}")
-print(f"El tamaño mínimo del TE es {min(dicc_analysis['TE'])}")
-print(f"El tamaño mínimo del TE es {min(dicc_analysis['TE2'])}")
-print(f"La cantidad total de TE es de {len(rec_TE)}")
-
-# AUXILIARY FUNCTIONS
-
+rec_TE = list(SeqIO.parse("data/TEDB.fasta", "fasta"))
+rec_non = list(SeqIO.parse("data/negative.fasta", "fasta"))
+index_negatives = [x for x in range(0, len(rec_non))]
 dicc_size = {0: 2000, 1: 2000, 2: 4000, 3: 1000, 4: 2000, 5: 3000, 6: 15000, 7: 18000}
 dicc_dom = {'LTR': 5, 'GAG': 1, 'PROT': 3, 'RT': 0, 'INT': 2, 'RH': 4, 'internal': 6, 'te': 7}
 dicc_sf = {'copia': 0, 'gypsy': 1}
 dicc = {'A': 0, 'a': 0, 'C': 1, 'c': 1, 'G': 2, 'g': 2, 'T': 3, 't': 3}
+
+
+def exploratory_analysis():
+    dicc_analysis = {'TE': [], 'TE2': [], 'PROT': [], 'RT': [], 'INT': [], 'RH': [],
+                     'GAG': [], 'LTR': [], 'internal': []}
+    for i in rec_TE:
+        dicc_tmp_dom = json.loads(i.description.split("#")[-1].replace('\'', '\"'))
+        dicc_tmp_te = json.loads(i.description.split("#")[-2].replace('\'', '\"'))
+        dicc_analysis['TE'].append(dicc_tmp_te['TE'][1]-dicc_tmp_te['TE'][0]+1)
+        dicc_analysis['TE2'].append(len(i.seq))
+        internal_list = []
+        for key, value in dicc_tmp_dom.items():
+            if key != 'LTR':
+                internal_list += value
+            if len(value) == 2:
+                dicc_analysis[key].append(value[-1]-value[0]+1)
+            else:
+                dicc_analysis[key].append(value[1]-value[0]+1)
+                dicc_analysis[key].append(value[3]-value[2]+1)
+        try:
+            dicc_tmp_dom['LTR']
+        except Exception:  # pylint: disable=broad-except
+            print(f'{i.description}')
+        start_internal = min(internal_list)
+        end_internal = max(internal_list)
+        dicc_analysis['internal'].append(end_internal-start_internal+1)
+
+    print(f"La cantidad total de RT es de {len(dicc_analysis['RT'])}")
+    print(f"La cantidad total de GAG es de {len(dicc_analysis['GAG'])}")
+    print(f"La cantidad total de INT es de {len(dicc_analysis['INT'])}")
+    print(f"La cantidad total de PROT es de {len(dicc_analysis['PROT'])}")
+    print(f"La cantidad total de RH es de {len(dicc_analysis['RH'])}")
+    print(f"La cantidad total de LTR es de {len(dicc_analysis['LTR'])}")
+
+    print(f"El tamaño máximo del RT es {max(dicc_analysis['RT'])}")
+    print(f"El tamaño máximo del GAG es {max(dicc_analysis['GAG'])}")
+    print(f"El tamaño máximo del INT es {max(dicc_analysis['INT'])}")
+    print(f"El tamaño máximo del PROT es {max(dicc_analysis['PROT'])}")
+    print(f"El tamaño máximo del RH es {max(dicc_analysis['RH'])}")
+    print(f"El tamaño máximo del LTR es {max(dicc_analysis['LTR'])}")
+
+    print(f"El tamaño mínimo del RT es {min(dicc_analysis['RT'])}")
+    print(f"El tamaño mínimo del GAG es {min(dicc_analysis['GAG'])}")
+    print(f"El tamaño mínimo del INT es {min(dicc_analysis['INT'])}")
+    print(f"El tamaño mínimo del PROT es {min(dicc_analysis['PROT'])}")
+    print(f"El tamaño mínimo del RH es {min(dicc_analysis['RH'])}")
+    print(f"El tamaño mínimo del LTR es {min(dicc_analysis['LTR'])}")
+
+    print(f"El tamaño máximo del internal es {max(dicc_analysis['internal'])}")
+    print(f"El tamaño mínimo del internal es {min(dicc_analysis['internal'])}")
+
+    print(f"El tamaño máximo del TE es {max(dicc_analysis['TE'])}")
+    print(f"El tamaño mínimo del TE es {min(dicc_analysis['TE'])}")
+    print(f"El tamaño mínimo del TE es {min(dicc_analysis['TE2'])}")
+    print(f"La cantidad total de TE es de {len(rec_TE)}")
 
 
 def Seq_to_2D(DNA_str):
@@ -185,95 +184,103 @@ def complete_with_negatives(Rep2D, Label, ventana):
     return np.append(Rep2D, DNA_faltante, axis=2), np.append(Label, label, axis=2)
 
 
-# Ventana
-ventana = 50000
-opcion = '.'
-sample = 722084
-step = 5000
-
-# DATASET GENERATION
-
-cont = 45000        # Empieza en el mismo valor donde quedo la vez pasada
-idx_initial = 172058    # Empieza en valor impreso y se le suma 1
-index_negatives = [x for x in range(0, len(rec_non))]
-
-X_data = np.zeros((1, 5, ventana), dtype=np.int8)
-Y_data = np.zeros((1, 3, int(ventana/100), 11), dtype=np.float32)
-Z_data = np.zeros((1, 10), dtype=np.int64)
-flag = 'first_in_seq'
-flag_save = 0
-cont_seq = 0
-for idx in range(idx_initial, len(rec_TE)):
-    item = rec_TE[idx]
-    try:
-        Rep2D_single, Label_single = DNA_dataset(item)
-    except Exception:  # pylint: disable=broad-except
-        print(f'La longitud de {item.description} es {len(item.seq)}')
-        continue
-    if flag == 'first_in_seq':
-        Rep2D = Rep2D_single
-        Label = Label_single
-        flag = 'other_in_seq'
-        Z_vector = np.zeros((1, 10), dtype=np.int64)
-        Z_vector[0, cont_seq] = idx
-        cont_seq += 1
-    elif flag == 'other_in_seq':
-        long_base = Rep2D.shape[2]
-        long_anexo = Rep2D_single.shape[2]
-        if long_base+long_anexo > ventana:
-            Rep2D, Label = complete_with_negatives(Rep2D, Label, ventana)
-            Y_data = np.append(Y_data, Label, axis=0)
-            X_data = np.append(X_data, Rep2D, axis=0)
+def dataset_creation():
+    global index_negatives
+    ventana = 50000
+    opcion = '.'
+    sample = 722084
+    step = 5000
+    cont = 0        # Empieza en el mismo valor donde quedo la vez pasada
+    idx_initial = 0    # Empieza en valor impreso y se le suma 1
+    X_data = np.zeros((1, 5, ventana), dtype=np.int8)
+    Y_data = np.zeros((1, 3, int(ventana/100), 11), dtype=np.float32)
+    Z_data = np.zeros((1, 10), dtype=np.int64)
+    flag = 'first_in_seq'
+    flag_save = 0
+    cont_seq = 0
+    gc.collect()
+    for idx in range(idx_initial, len(rec_TE)):
+        gc.collect()
+        item = rec_TE[idx]
+        try:
+            Rep2D_single, Label_single = DNA_dataset(item)
+        except Exception:  # pylint: disable=broad-except
+            print(f'La longitud de {item.description} es {len(item.seq)}')
+            continue
+        if flag == 'first_in_seq':
             Rep2D = Rep2D_single
             Label = Label_single
-            cont += 1
-            Z_data = np.append(Z_data, Z_vector, axis=0)
+            flag = 'other_in_seq'
             Z_vector = np.zeros((1, 10), dtype=np.int64)
-            cont_seq = 0
             Z_vector[0, cont_seq] = idx
             cont_seq += 1
-            flag_save = 1
-            index_negatives = [x for x in range(0, len(rec_non))]
-        else:
-            Rep2D = np.append(Rep2D, Rep2D_single, axis=2)
-            Label = np.append(Label, Label_single, axis=2)
-            Z_vector[0, cont_seq] = idx
-            cont_seq += 1
-    if (idx % 100) == 0:
-        print(idx)
-    if (cont % step == 0) and (flag_save == 1):
-        seed = 7
-        X_train, X_test_dev, Y_train, Y_test_dev, Z_train, Z_test_dev = train_test_split(
-          X_data[1:, :, :], Y_data[1:, :, :, :], Z_data[1:, :], test_size=0.2, random_state=seed
-        )
-        X_dev, X_test, Y_dev, Y_test, Z_dev, Z_test = train_test_split(
-          X_test_dev, Y_test_dev, Z_test_dev, test_size=0.5, random_state=seed
-        )
-        print(X_train.shape)
-        print(Y_train.shape)
-        print(X_dev.shape)
-        print(Y_dev.shape)
-        print(X_test.shape)
-        print(Y_test.shape)
-        print('idx: '+str(idx))
-        print('cont: '+str(cont))
-        np.save(opcion+'/X_train{}.npy'.format(cont), X_train)
-        np.save(opcion+'/Y_train{}.npy'.format(cont), Y_train)
-        np.save(opcion+'/Z_train{}.npy'.format(cont), Z_train)
+        elif flag == 'other_in_seq':
+            long_base = Rep2D.shape[2]
+            long_anexo = Rep2D_single.shape[2]
+            if long_base+long_anexo > ventana:
+                Rep2D, Label = complete_with_negatives(Rep2D, Label, ventana)
+                Y_data = np.append(Y_data, Label, axis=0)
+                X_data = np.append(X_data, Rep2D, axis=0)
+                Rep2D = Rep2D_single
+                Label = Label_single
+                cont += 1
+                Z_data = np.append(Z_data, Z_vector, axis=0)
+                Z_vector = np.zeros((1, 10), dtype=np.int64)
+                cont_seq = 0
+                Z_vector[0, cont_seq] = idx
+                cont_seq += 1
+                flag_save = 1
+                index_negatives = [x for x in range(0, len(rec_non))]
+            else:
+                Rep2D = np.append(Rep2D, Rep2D_single, axis=2)
+                Label = np.append(Label, Label_single, axis=2)
+                Z_vector[0, cont_seq] = idx
+                cont_seq += 1
+        if (idx % 100) == 0:
+            print(idx)
+        if (cont % step == 0) and (flag_save == 1):
+            seed = 7
+            X_train, X_test_dev, Y_train, Y_test_dev, Z_train, Z_test_dev = train_test_split(
+              X_data[1:, :, :], Y_data[1:, :, :, :], Z_data[1:, :], test_size=0.2, random_state=seed
+            )
+            X_dev, X_test, Y_dev, Y_test, Z_dev, Z_test = train_test_split(
+              X_test_dev, Y_test_dev, Z_test_dev, test_size=0.5, random_state=seed
+            )
+            print(X_train.shape)
+            print(Y_train.shape)
+            print(X_dev.shape)
+            print(Y_dev.shape)
+            print(X_test.shape)
+            print(Y_test.shape)
+            print('idx: '+str(idx))
+            print('cont: '+str(cont))
+            np.save(opcion+'/X_train{}.npy'.format(cont), X_train)
+            np.save(opcion+'/Y_train{}.npy'.format(cont), Y_train)
+            np.save(opcion+'/Z_train{}.npy'.format(cont), Z_train)
 
-        np.save(opcion+'/X_dev{}.npy'.format(cont), X_dev)
-        np.save(opcion+'/Y_dev{}.npy'.format(cont), Y_dev)
-        np.save(opcion+'/Z_dev{}.npy'.format(cont), Z_dev)
+            np.save(opcion+'/X_dev{}.npy'.format(cont), X_dev)
+            np.save(opcion+'/Y_dev{}.npy'.format(cont), Y_dev)
+            np.save(opcion+'/Z_dev{}.npy'.format(cont), Z_dev)
 
-        np.save(opcion+'/X_test{}.npy'.format(cont), X_test)
-        np.save(opcion+'/Y_test{}.npy'.format(cont), Y_test)
-        np.save(opcion+'/Z_test{}.npy'.format(cont), Z_test)
+            np.save(opcion+'/X_test{}.npy'.format(cont), X_test)
+            np.save(opcion+'/Y_test{}.npy'.format(cont), Y_test)
+            np.save(opcion+'/Z_test{}.npy'.format(cont), Z_test)
 
-        X_data = np.zeros((1, 5, ventana), dtype=np.int8)
-        Y_data = np.zeros((1, 3, int(ventana/100), 11), dtype=np.float32)
-        Z_data = np.zeros((1, 10), dtype=np.int64)
-        flag_save = 0
-    if cont == sample:
-        break
-print(cont)
-print(item.description)
+            X_data = np.zeros((1, 5, ventana), dtype=np.int8)
+            Y_data = np.zeros((1, 3, int(ventana/100), 11), dtype=np.float32)
+            Z_data = np.zeros((1, 10), dtype=np.int64)
+            flag_save = 0
+            gc.collect()
+        if cont == sample:
+            break
+    print(cont)
+    print(item.description)
+
+
+def main():
+    exploratory_analysis()
+    dataset_creation()
+
+
+if __name__ == "__main__":
+    main()
