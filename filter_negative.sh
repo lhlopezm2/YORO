2ffee7caaeee0fbc3abc25db7b6da16493e5a6a5
Luis <lhlopezm2@gmail.com>
2023-08-01 07:35:34 -0500

    Add bash file to execute filter_negative.py

---
+++ filter_negative.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+#SBATCH -J filter
+#SBATCH -D .
+#SBATCH -o out_filter_negative.txt
+#SBATCH -e err_filter_negative.txt
+#SBATCH -n 5
+#SBATCH -N 1
+#SBATCH --partition=long
+#SBATCH --mem=10G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=luish.lopezm@autonoma.edu.co
+
+source ~/.bashrc
+conda activate bioinfo
+/shared/home/sorozcoarias/anaconda3/bin/time -f 'Elapsed time: %e s - memory used: %M kb - CPU used: %P' python filter_negative.py 
